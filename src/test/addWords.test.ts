c61cf064a1b649b52b71cc4a4ac443de6cc337e5
Jacob Habib <habib.50@osu.edu>
2023-09-25 01:10:36 -0500

    mocha tests working, add prettier

--- src/test/addWords.test.ts
+++ src/test/addWords.test.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { Filter } from '../badwords';
+import { Filter } from '../badwords.js';
 
 describe('filter', function () {
     describe('addWords', function () {
