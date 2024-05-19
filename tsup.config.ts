5286598cf0bc553c22f9fbf3bfb436a2807568a3
Jacob Habib <jahgoreds@gmail.com>
2024-05-18 20:27:54 -0500

    new release, prettier

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ['src/index.ts'],
-  format: ['cjs', 'esm'],
+  entry: ["src/index.ts"],
+  format: ["cjs", "esm"],
   dts: true,
   sourcemap: true,
-  outDir: 'dist',
-  target: 'es2016',
+  outDir: "dist",
+  target: "es2016",
   splitting: false,
   clean: true,
 });
